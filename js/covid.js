7727f0601f9eab75b33ff62705dc85623d36f767
Alfred Hutomo <62414727+AlfredHutomo-Ichida@users.noreply.github.com>
2020-10-02 12:15:14 +1000

    updates on covid.js and activit1.html

--- js/covid.js
+++ js/covid.js
@@ -1,28 +1,46 @@
-function correct() {
-    number = document.getElementById("counter").value;
-    score = parseInt(number);
-    score = score + 1;
-    document.getElementById("counter").value = score;
-}
+$(document).ready(function () {
+    $.getJSON("data/player.json", function (data) {
+        var name, score, info;
 
-function wrong() {
-    number = document.getElementById("counter").value;
-    score = parseInt(number);
-    score = score - 1;
-    document.getElementById("counter").value = score;
-}
+        // calling JSON object and finding the name of player and its score
+        $(data.players).each(function (index, value) {
+            if (value.name == "Joe") {
+                name = value.name;
+                score = value.score;
+            }
+        });
+        // prep the values
+        info = "Name: " + name + "\nScore: " + score;
 
-function validate(text, answer) {}
-
-function spellcheck(txt, answer) {
-    this.txt = txt;
-    this.txt = {};
-    txt.forEach(item => {
-        !this.txt[item] && (this.txt[item] = 0);
-        this.txt[item]++;
+        $(".info").append(info);
     });
-}
 
-$(function() {
-    $('body').removeClass('fade-out');
+    function correct() {
+        number = document.getElementById("counter").value;
+        score = parseInt(number);
+        score = score + 1;
+        document.getElementById("counter").value = score;
+    }
+
+    function wrong() {
+        number = document.getElementById("counter").value;
+        score = parseInt(number);
+        score = score - 1;
+        document.getElementById("counter").value = score;
+    }
+
+    function validate(text, answer) {}
+
+    function spellcheck(txt, answer) {
+        this.txt = txt;
+        this.txt = {};
+        txt.forEach(item => {
+            !this.txt[item] && (this.txt[item] = 0);
+            this.txt[item]++;
+        });
+    }
+
+    $(function () {
+        $("body").removeClass("fade-out");
+    });
 });
