84e5b2ed1f188ec96f55b28760598a1e6200a214
Alfred Hutomo <62414727+AlfredHutomo-Ichida@users.noreply.github.com>
2020-10-14 20:58:00 +1100

    new functions.js

--- js/covid.js
+++ js/covid.js
@@ -75,30 +75,5 @@ $(document).ready(function () {
       $("body").removeClass("fade-out");
    });
 
-   function nameEntered() {
-      var userName = document.getElementById("name").value;
-      var message = document.getElementById("welcome");
-
-      if (userName != "") {
-         score = 0;
-         localStorage.setItem("user", userName);
-         localStorage.setItem("userScore", String(score));
-         message.innerHTML = "Hello " + userName + "! Hope you are having a great day! Let's go onto the next activity!";
-         document.getElementById("nxt-btn").className = "show";
-      } else {
-         alert("Enter your name");
-      }
-   }
-
-   function displayName() {
-      document.getElementById("name").innerHTML = localStorage.getItem("user");
-      name.innerHTML = "" + user;
-   }
-
-   function displayScore() {
-      document.getElementById("score").innerHTML = localStorage.getItem("userScore");
-      score.innerHTML = "" + userScore;
-   }
-
    // do not write any fuction below this point
 });
